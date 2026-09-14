be748fb21dd6c7800e7f589f43879294dc93805b
hriday666 <bbadhon60@gmail.com>
2026-09-14 15:25:51 +0600

    sw.js

---
+++ sw.js
@@ -0,0 +1,55 @@
+self.addEventListener("push", event => {
+  let data = {};
+
+  try {
+    data = event.data ? event.data.json() : {};
+  } catch (e) {
+    data = {
+      title: "Private Chat",
+      body: event.data ? event.data.text() : "New notification"
+    };
+  }
+
+  const title = data.title || "Private Chat";
+
+  const options = {
+    body: data.body || "You have a new notification",
+    icon: data.icon || "./icon-192.png",
+    badge: data.badge || "./icon-192.png",
+    data: {
+      url: data.url || "./"
+    },
+    vibrate: [200, 100, 200],
+    tag: data.tag || "private-chat"
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  );
+});
+
+
+self.addEventListener("notificationclick", event => {
+  event.notification.close();
+
+  const url = event.notification.data?.url || "./";
+
+  event.waitUntil(
+    clients.matchAll({
+      type: "window",
+      includeUncontrolled: true
+    }).then(clientList => {
+
+      for (const client of clientList) {
+        if ("focus" in client) {
+          client.navigate(url);
+          return client.focus();
+        }
+      }
+
+      if (clients.openWindow) {
+        return clients.openWindow(url);
+      }
+    })
+  );
+});
